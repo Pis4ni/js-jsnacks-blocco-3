9255ec04c8e5b0c600842944dfe4abcc715ba2fd
Pis4ni <pis4wrk@gmail.com>
2023-07-26 15:30:21 +0200

    add: snack1

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-//chiedo all utente il primo numero
+const min = prompt('metti un numero'); // chiedo all utente il primo numero
+const max = prompt('meti un num'); // chiedo all utente il secondo numero
+const random = Math.floor(Math.random() * (max - min)+ min );//creo un numro casuale compreso tra i due numeri cheisti all utente
+console.log(random);
