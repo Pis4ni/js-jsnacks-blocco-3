d2a90dbabfadcb82364d72f6cb4738c1eb77aa30
Pis4ni <pis4wrk@gmail.com>
2023-07-26 18:02:24 +0200

    fix: errors

--- README.md
+++ README.md
@@ -32,3 +32,34 @@ Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Contin
 - chiedo all utente un numero
 - **FINCHE**la somma del valore degli elementi dell array non supera 50
 - chiedo all utente un numero
+- **SE** il totale della somma degli elementi nell array é inferiore a 50
+    - aggiungo il numero all array
+
+## TRACCIA 4
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+Calcola la media dei numeri inseriti.
+### SVOLGIMENTO
+- creo un array vuoto
+- chiedo all utente un numero
+- **FINCHE**la somma del valore degli elementi dell array non supera 50
+- chiedo all utente un numero
+- **SE** il totale della somma degli elementi nell array é inferiore a 50
+    - aggiungo il numero all array
+- calcolo la media dei numeri inseriti
+
+
+## TRACCIA 5
+
+CORREGGI QUESTO CODICE:
+--------------------------
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = "";
+    for (let i = 0; i < numbers.length - 1; i++;) {
+        if (numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+    }
+    console.log(evenNumbers);
+
+    // dovrebbe restituire [1,3,5]	
+--------------------------
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// chiedo all utente il primo numero
+//!=========================================================================================================================
+/* // chiedo all utente il primo numero
 const min = parseInt(prompt('metti un numero'));
 // chiedo all utente il secondo numero
 const max = parseInt(prompt('meti un num')); 
 //creo un numro casuale compreso tra i due numeri cheisti all utente
-const random = Math.floor(Math.random() * (max - min)+ min );
+const random = 0;
 // stampo il risultato a schermo
 console.log(random);
 
@@ -20,9 +21,11 @@ if (min>max) {
     // stampo il risultato a schermo
     console.log(random)
     
-}
+} */
 
-//chiedo all utente la prima parola
+//!=========================================================================================================================
+
+/* //chiedo all utente la prima parola
 const w1= prompt('prima parola')
 //chiedo all utente la seconda parola
 const w2= prompt('seconda parola')
@@ -37,20 +40,61 @@ if (w1.length==w2.length) {
     console.log(w1)
 }else{
     console.log(w2)
-}
+} */
 
+//!=========================================================================================================================
 
-//creo un array vuoto
+/* //creo un array vuoto
 let empty = []
 // immagazino il totale
-let total = 
+let total = 0
 // FINCHÉ il totale della somma degli elementi é inferiore a 50
 while (total<50) {
     //chiedo all utente un numero
-    number = parseInt(prompt('inserisci un numero'))
-    somma += number
-    if(total<50){
-        empty.push(number)
-    }
+    let number = parseInt(prompt('inserisci un numero'))
+    total += number
+    empty.push(number)
 
+    console.log(number)
+    console.log(total)
+    console.log(empty)
 }
+ */
+
+
+//!=========================================================================================================================
+/* //creo un array vuoto
+let empty = []
+// immagazino il totale
+let total = 0
+// FINCHÉ il totale della somma degli elementi é inferiore a 50
+while (total<50) {
+    //chiedo all utente un numero
+    let number = parseInt(prompt('inserisci un numero'))
+    total += number
+    empty.push(number)
+
+    console.log(number)
+    console.log(total)
+    console.log(empty)
+}
+
+
+//calcolo la media dei numeri inseriti
+let media = total / empty.length  
+console.log(media) */
+
+//!=========================================================================================================================
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let evenNumbers = [];
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] %2 !== 0 && numbers[i] < 7) {
+        evenNumbers.push(numbers[i]) ;
+    }
+}
+
+console.log(evenNumbers)
+// dovrebbe restituire [1,3,5]	
+
+
