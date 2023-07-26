c5e3629625421428fd6134071794415d71dd31d0
Pis4ni <pis4wrk@gmail.com>
2023-07-26 15:50:07 +0200

    add: conditional control

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ const max = prompt('meti un num');
 const random = Math.floor(Math.random() * (max - min)+ min );
 // stampo il risultato a schermo
 console.log(random);
+
+
+if (min>max) {
+    //creo un numro casuale compreso tra i due numeri cheisti all utente
+    const random = Math.floor(Math.random() * (max - min)+ min );
+    // stampo il risultato a schermo
+    console.log(random)
+
+}else{
+    //creo un numro casuale compreso tra i due numeri cheisti all utente
+    const random = Math.floor(Math.random() * (min - max)+ max );
+    // stampo il risultato a schermo
+    console.log(random)
+    
+}
